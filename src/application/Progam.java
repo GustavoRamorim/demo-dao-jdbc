c06ab57027efc910315adec70a7bb45a51981396
Gustavo Rabelo <gustavo.amorim09@hotmail.com>
2024-05-24 17:18:29 -0300

    findAll implemetation

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -14,20 +14,21 @@ public class Progam {
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findById =====");
-		
 		Seller seller = sellerdao.findById(3);
-		
 		System.out.println(seller);
 		
+		System.out.println();
 		System.out.println("=== TEST 2: seller findByDepartment =====");
 		Department department = new Department (2, null);
 		List<Seller> list = sellerdao.findByDepartment(department);
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
-		
-		
-		
+		System.out.println();
+		System.out.println("=== TEST 3: seller findAll =====");
+		list = sellerdao.findAll();
+		for (Seller obj: list) {
+			System.out.println(obj);
+		}	
 	}
-
 }
